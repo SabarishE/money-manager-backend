f17507461ad4aa8bad05b75cb3c59724bc28ccc0
SabarishE <sabarishtrblzr@gmail.com>
2021-07-06 14:19:46 +0530

    2.Heroky url

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 app.listen(PORT,console.log("server started"));
 
-// const url= process.env.MONGODB_URI || "mongodb://localhost/trackerDB";
-const url= "mongodb+srv://SabarishE:sabarishe@cluster0.eeimf.mongodb.net/trackerDB";
+ const url= process.env.MONGODB_URI || "mongodb://localhost/trackerDB";
+// const url= "mongodb+srv://SabarishE:sabarishe@cluster0.eeimf.mongodb.net/trackerDB";
 mongoose.connect(url,{useNewUrlParser:true});
 
 const con=mongoose.connection;
