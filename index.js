fc98ddba3e6dcb0d3e185ce67ed8b4c3f55b509c
SabarishE <sabarishtrblzr@gmail.com>
2021-07-06 18:45:00 +0530

    cors commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import router from "./tracker-route.js";
 
 const app=express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.listen(PORT,console.log("server started"));
