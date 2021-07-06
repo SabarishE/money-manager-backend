1a5de80dd4a35bfd9feef61d3051870eb04914cd
SabarishE <sabarishtrblzr@gmail.com>
2021-07-06 21:13:46 +0530

    DELETE commit

--- tracker-route.js
+++ tracker-route.js
@@ -24,13 +24,25 @@ router.post("/",async(req,res)=>{
 
 })
 
-// router.delete("/:id",async(req,res)=>{
+router.delete("/:id",async(req,res)=>{
 
-//     const { id }= req.params;
-    
-//     const data = await Tracker.findById(id);
-//     await data.remove();
-//     res.send({...data,message:"Entry is deleted"});
-// })
+    const { id }= req.params;
+    console.log(id);
+  
+    try {
+  
+    const data = await Tracker.findById(id);
+    await user.remove();
+    res.send({...data,message:"data is deleted"});
+    console.log("data with above ID is deleted");
+    }
+   catch(err)
+   {
+     res.status(500);
+     res.send("data not found in trackerDB");
+     console.log("err: user not found");
+   }
+  
+  })
   
   export default router;
