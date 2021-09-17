ab36b5a419659415b34226477c8c5b171f58f960
SabarishE <sabarishtrblzr@gmail.com>
2021-09-17 20:27:08 +0530

    x-auth-commit

--- auth.js
+++ auth.js
@@ -3,14 +3,24 @@ import jwt from "jsonwebtoken"
 
 export const auth=(req,res,next)=>{
 
-    try{
-const token =req.header("x-auth-token");
-console.log("live user token >>>",token);
-jwt.verify(token,"mysecretkey");
-next();
+        const token =req.header("x-auth-token");
+        req.token=token;
+        jwt.verify(req.token,"mysecretkey",async(err,data)=>{
+
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            console.log("auth success !!!",data);
+            next();
+        }
+        
+       
+        });
+     
 }
-catch(err){
-    console.log("error in token match >>>",err)
-    res.status(500).send({error:err,message:"user not matched"});
-}
-}
+
+
+
+
+
